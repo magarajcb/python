8fd89fb9a9ea62989b058cf57824623a18c8969f
magaraj <magarajcb@gmail.com>
2026-08-21 13:20:08 +0530

    max and min using for...in loop

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-num=int(input("Enter ur num:"))
-if num>0:
-    print('positive')
-elif num==0:
-    print("Zero")
-else:
-    print("Negative")
+marks=["31","25","77","81","97"]
+high=marks[0]
+for a in marks:
+    if a>high:
+        high=a
+
+print(high)
